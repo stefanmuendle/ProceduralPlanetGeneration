d0237f87e7586221af89dbe7fcbe8497beb56a0c
stefanmuendle <131877125+stefanmuendle@users.noreply.github.com>
2024-11-17 20:21:43 +0100

    EP.2 - WITHOUT UPDATING INSTANTLY

--- Planet.gd
+++ Planet.gd
@@ -1,8 +1,16 @@
+@tool
 extends Node3D
 
+@export var planet_data: Resource:
+	set(val):
+		planet_data = val
+		on_data_changed()
 
-func _ready():
+func _ready() -> void:
+	on_data_changed()
+
+func on_data_changed() -> void:
 	for child in get_children():
-		var face := child as PlanetMeshFace
-		face.regenerate_mesh()
-
+		var face = child as PlanetMeshFace
+		if face:
+			face.regenerate_mesh(planet_data)
---
+++ PlanetData.gd
@@ -0,0 +1,12 @@
+@tool
+extends Resource
+
+class_name PlanetData
+
+@export var radius := 1.0 : 
+	set(val): 
+		radius = val
+
+@export var resolution := 10.0 : 
+	set(val): 
+		resolution = val
